eb2ecdf9d11cacb4d0092a495ffaf0acdad51b86
diegorosasr <diegorosas.r22@gmail.com>
2021-10-14 17:58:26 +0000

    Act. Sistemas Operativos

---
+++ numarch.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+int main(){
+    int fd[2];
+    int pid;
+    pipe(fd);
+    pid = fork();
+    if (pid == 0){
+        close(STDIN_FILENO);
+        dup(fd[0]);
+        close(fd[1]);
+        execlp("wc","wc","-1",NULL);
+    } else{
+        close(STDOUT_FILENO);
+        dup(fd[1]);
+        close(fd[0]);
+        execlp("ls","ls",NULL);
+    }
+    return 0;
+}
